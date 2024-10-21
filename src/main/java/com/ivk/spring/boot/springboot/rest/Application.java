9f3e3eadfde7111fee8617a372878d998454414f
fork_IVK <ivkor72@mail.ru>
2024-10-21 03:12:41 +0300

    Initial commit

---
+++ src/main/java/com/ivk/spring/boot/springboot/rest/Application.java
@@ -0,0 +1,13 @@
+package com.ivk.spring.boot.springboot.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
